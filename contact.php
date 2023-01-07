1e48c879c3260b00616d866aab12cdf43666b809
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-01-07 20:46:11 +0800

    :sparkles: Contact.php done-ish :sparkles:

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php 
+
+if(isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $mailTo = "jkrbn99@gmail.com";
+  $headers = "From: ".$email;
+  $txt = "You have recieved an email from ".$name.".\n\n".$message;
+
+  mail($mailTo, $txt, $headers);
+header("Location: index.php?mailsend");
+}
