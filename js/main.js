95f604a537fa03b7a303f85dc0154cf46869a3bc
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-03-05 18:45:25 +0800

    Hacked Text Effect [Logo]

--- js/main.js
+++ js/main.js
@@ -35,6 +35,38 @@ $(document).ready(function () {
     }
     
 
+    	//********** Hacked Text Effect [Logo]
+
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let interval = null;
+    
+        document.querySelector(".logo").onmouseover = event => {  
+        let iteration = 0;
+        
+        clearInterval(interval);
+        
+        interval = setInterval(() => {
+            event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter, index) => {
+                if(index < iteration) {
+                return event.target.dataset.value[index];
+                }
+            
+                return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+            
+            if(iteration >= event.target.dataset.value.length){ 
+            clearInterval(interval);
+            }
+            
+            iteration += 1 / 3;
+        }, 30);
+    }
+
+    
 	//********** menu hides after click (mobile menu)
 
 	$(document).on('click', '.navbar-collapse.in', function (e) {
